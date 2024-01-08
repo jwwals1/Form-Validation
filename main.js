3a28468a5fa0c6e7833cf31b1fe51318795b28a7
jwwals1 <joe_walsh59@yahoo.com>
2024-01-08 13:47:05 -0600

    added country validation logic

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const email = document.getElementById("email")
 const form = document.querySelector("form");
 const emailError = document.querySelector("#email + span.error");
+const country = document.getElementById('country')
+const countryError = document.querySelector("#country + span.error")
+
 
 email.addEventListener("input", (event) => {
     if (email.validity.valid) {
@@ -11,8 +14,21 @@ email.addEventListener("input", (event) => {
     }
 });
 
+country.addEventListener("input", (event) => {
+    if (country.validity.valid) {
+        countryError.textContent = ""
+        countryError.className = "error"
+    } else {
+        showError();
+    }
+})
+
 form.addEventListener("submit", (event) => {
     if (!email.validity.valid) {
+        showError()
+        event.preventDefault()
+    }
+    else if (!country.validity.valid) {
         showError()
         event.preventDefault()
     }
@@ -25,6 +41,15 @@ function showError() {
         emailError.textContent = "You need to enter a valid email address"
     } else if (email.validity.tooShort) {
         emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`
+    }
+    else if (country.validity.valueMissing) {
+        countryError.textContent = "You need to enter a country"
+    }
+    else if (country.validity.typeMismatch) {
+        countryError.textContent = "You need to enter a valid country"
+    }
+    else if (country.validity.tooShort) {
+        countryError.textContent = `Country should be at least ${country.minLength} characters; you entered ${country.value.length}.`
     }
     emailError.className = "error active"
 }
